3f52412b5145cab34747d6d1a93cb955afa9f79b
Samuel Doud <samueldoud@me.com>
2023-04-28 18:07:08 -0400

    correcting mimic4 loader

--- data/mimiciv.py
+++ data/mimiciv.py
@@ -1,28 +1,28 @@
 from os import path
 from typing import List, Optional
 
-from pyhealth.datasets import MIMIC3Dataset
+from pyhealth.datasets import MIMIC4Dataset
 
 from pyhealth.tasks import mortality_prediction_mimic3_fn, length_of_stay_prediction_mimic3_fn
 
-MIMIC_PATH = "./data/MIMIC4"
+MIMIC_PATH = "./data/MIMIC4/hosp"
 
 
 def get_mimic_iv(
         tables: Optional[List[str]] = ["DIAGNOSES_ICD", "PROCEDURES_ICD", "PRESCRIPTIONS"],
         code_mapping: Optional[List[str]] = {"ICD9CM": "CCSCM"},
-) -> MIMIC3Dataset:
+) -> MIMIC4Dataset:
     if not path.isdir(MIMIC_PATH):
         from personal_info import PhysioNet
         url = f"https://{PhysioNet.user_name}:{PhysioNet.password}@physionet.org/files/mimiciv/2.2/"
         # wget.download(f"{url}" , out=MIMIC_PATH)
 
-    mimic3base = MIMIC3Dataset(
+    mimic4base = MIMIC4Dataset(
         root=MIMIC_PATH,
         tables=tables,
         code_mapping=code_mapping,
     )
-    return mimic3base
+    return mimic4base
 
 
 def get_mortality_dataset():
@@ -31,3 +31,6 @@ def get_mortality_dataset():
 def get_length_of_stay_dataset():
     return get_mimic_iv().set_task(length_of_stay_prediction_mimic3_fn)
 
+if __name__ == "__main__":
+    dataset = get_mimic_iv()
+    print(dataset)
